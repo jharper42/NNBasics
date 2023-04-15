d56c48ec9d8362c3cdba5c27d1c36b675498964f
jharper42 <jesseharper42@gmail.com>
2023-04-15 12:46:29 -0500

    created basic layer class which creates a layer between 2 columns of nodes and a basic method to forward propagate.

--- NNBasics.py
+++ NNBasics.py
@@ -1,1 +1,27 @@
+import numpy as np
+import math
+import typing
 
+class layer:
+    def __init__(self, l_nodes:int, r_nodes:int):
+        self.weights = np.matrix(np.random.rand(r_nodes,l_nodes))
+        self.biases = np.matrix(np.random.rand(1,r_nodes))
+
+    def forward(self, A:np.matrix) -> np.matrix: 
+        return (np.dot(A,self.weights.T) + self.biases)
+
+
+if __name__ == '__main__':
+    np.random.seed(1)
+
+    X = np.matrix([1,0,0,0]).T
+    layer1 = layer(1, 3)
+    R1 = layer1.forward(X)
+
+    layer2 = layer(3, 2)
+    R2 = layer2.forward(R1)
+
+    layer3 = layer(2,1)
+    R3 = layer3.forward(R2)
+    print(R3)
+    
